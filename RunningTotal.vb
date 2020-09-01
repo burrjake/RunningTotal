32c11122b052bcc7c3685c5f21d845abc64c76a6
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-09-01 08:18:15 -0600

    changed running total to decimal

--- RunningTotal.vb
+++ RunningTotal.vb
@@ -1,12 +1,15 @@
 ﻿Option Strict On
 Option Explicit On
+Option Compare Text
+Imports System.Linq.Expressions
 
 Module RunningTotal
 
     Sub Main()
         Dim userInput As String
-        Dim runningTotal As Integer
+        Dim runningTotal As Decimal
         Const TAXRATE As Decimal = 0.06D
+        Dim badData As Boolean = True
 
         Console.WriteLine("Please select an option.")
         Console.WriteLine("1. Add item")
@@ -18,25 +21,27 @@ Module RunningTotal
 
         While userInput <> "q"
             userInput = Console.ReadLine()
-
-            If userInput = "1" Then
-
-
+            Do
+                If userInput = "1" Then
 
 
-                Try
-                    'runningTotal = runningTotal + CInt(userInput)
-                    runningTotal += CInt(userInput)
 
-                Catch ex As Exception
-                    'prompt usr to give good data
-                End Try
-            ElseIf userInput = "2" Then
-                Console.WriteLine("Tax: " runningTotal * TAXRATE)
-            Else
 
-            End If
+                    Try
+                        'runningTotal = runningTotal + CInt(userInput)
+                        runningTotal += CDec(userInput)
+                        badData = False
+                    Catch ex As Exception
+                        'prompt usr to give good data
+                        Console.ReadLine("Please enter a number")
+                        badData = True
+                    End Try
+                ElseIf userInput = "2" Then
+                    Console.WriteLine("Tax: " & runningTotal * TAXRATE)
+                Else
 
+                End If
+            Loop While badData
 
 
             Try
@@ -48,7 +53,7 @@ Module RunningTotal
             End Try
 
             Console.WriteLine("Total: " & runningTotal)
-            Console.WriteLine("Tax: " runningTotal * TAXRATE)
+            Console.WriteLine("Tax: " & runningTotal * TAXRATE)
         End While
     End Sub
 
